eb93aef4cae308e1f0ee80b9edd7f0c0aa0af99e
Gongchaeyeon <dnjftjd53@naver.com>
2020-06-24 08:02:16 +0900

    Issue #6 Modify file sum.py(Remove Unnecessart) and sum.conf(path modification)

--- calc/sum.py
+++ calc/sum.py
@@ -1,5 +1,4 @@
 from cgi import parse_qs
-import matplotlib.pyplot as plt
 from template2 import html
 
 def application(environ, start_response):
@@ -7,19 +6,17 @@ def application(environ, start_response):
         d = parse_qs(environ['QUERY_STRING'])
         a = d.get('a', [''])[0]
         b = d.get('b', [''])[0]
-
-        sum, mul = 0, 0
-
-	if '' not in [a,b]:
-		a,b = int(a), int(b)
 	
-                sum = a+b
-                mul= a*b
-
+	sum, mul = 0,0
+	if '' not in[a,b]:
+		a,b = int(a), int(b)
+		sum = a+b
+		mul = a*b
+	
         response_body = html % {
 
-                'sum': sum,
-                'mul' : mul
+                'sum': a+b,
+                'mul' :a*b 
         }
 
         start_response('200 OK', [
