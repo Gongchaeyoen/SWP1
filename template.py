b5c4861767a959fada24954809680be0eb594364
Gongchaeyeon <dnjftjd53@naver.com>
2020-06-23 11:27:05 +0900

    add a file

---
+++ template.py
@@ -0,0 +1,14 @@
+html = b"""
+<html>
+        <body>
+          <form method = "get">
+                a = <input type = "number" name = "a">
+                b = <input type = "number" name = "b"> <br><br>
+             <input type = "submit">
+
+         </form>
+                sum : %(sum)s </br>
+                multiplication : %(mul)s </br>
+        </body>
+</html>
+"""
